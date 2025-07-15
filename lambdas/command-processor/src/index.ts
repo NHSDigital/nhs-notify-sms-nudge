701c22b73abcfb9af894bc883cd02350d79ae6ba
mark.slowey1 <mark.slowey1@nhs.net>
2025-07-15 15:56:23 +0100

    CCM-10151: command processor stub

--- lambdas/command-processor/src/index.ts
+++ lambdas/command-processor/src/index.ts
@@ -1,7 +1,10 @@
 import type { SQSHandler } from 'aws-lambda';
 
-export const handler: SQSHandler = async (event) => {
-  for (const record of event.Records) {
-    console.log('Received SQS message:', record.body);
-  }
+import { SQSEvent } from 'aws-lambda';
+import { logger } from 'nhs-notify-sms-nudge-utils';
+
+const SEND_MESSAGE_URL = process.env.SEND_MESSAGE_URL!;
+
+export const handler: SQSHandler = async (_event: SQSEvent) => {
+  logger.info('Received event. Would target %s', SEND_MESSAGE_URL);
 };
