316b7f3840c48b8cd77e8e685732044ffa7cfc63
mark.slowey1 <mark.slowey1@nhs.net>
2025-07-15 15:40:32 +0100

    CCM-10151: Basic command infra

--- lambdas/command-processor/src/command-processor.ts
+++ lambdas/command-processor/src/command-processor.ts
@@ -3,5 +3,10 @@
 // The handler should be exported with CJS syntax for compatibility with OpenTelemetry / X-Ray
 
 import { SQSEvent } from 'aws-lambda';
+import { logger } from 'nhs-notify-sms-nudge-utils';
 
-export const handler = async (_event: SQSEvent) => {};
+const SEND_MESSAGE_URL = process.env.SEND_MESSAGE_URL!;
+
+export const handler = async (_event: SQSEvent) => {
+  logger.info('Received event. Would target %s', SEND_MESSAGE_URL);
+};
