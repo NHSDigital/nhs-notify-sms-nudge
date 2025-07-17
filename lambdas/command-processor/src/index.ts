62c670321c1820c2b67b05b48db4b39f0d2a9135
Ian Hodges <ian.hodges1@nhs.net>
2025-07-17 15:49:47 +0100

    CCM-10151: add extra logging to the lambda stub

--- lambdas/command-processor/src/index.ts
+++ lambdas/command-processor/src/index.ts
@@ -1,13 +1,16 @@
 import type { SQSHandler } from 'aws-lambda';
-
 import { SQSEvent } from 'aws-lambda';
 import { logger } from 'nhs-notify-sms-nudge-utils';
 
 const SEND_MESSAGE_URL = process.env.SEND_MESSAGE_URL!;
 
 export const handler: SQSHandler = async (event: SQSEvent) => {
+  const body = event.Records[0].body;
+  const nudgeCommand = JSON.parse(body);
+
   logger.info('Received event', {
-    message0Id: event.Records[0].messageId,
+    messageId: event.Records[0].messageId,
+    sourceEventId: nudgeCommand.sourceEventId,
     target: SEND_MESSAGE_URL,
   });
 };
