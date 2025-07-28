8d2fa4fae3ae47ab2a35d5f796d72fde1fc42100
simonlabarere <simon.labarere1@nhs.net>
2025-07-28 08:33:41 +0100

    CCM-10152: APIM Token lambdas

--- infrastructure/terraform/components/nudge/ssm_parameter_api_key.tf
+++ infrastructure/terraform/components/nudge/ssm_parameter_api_key.tf
@@ -2,9 +2,7 @@ resource "aws_ssm_parameter" "api_key" {
   name = local.apim_api_key_ssm_parameter_name
   description = "Access token for APIM"
   type        = "SecureString"
-  value = jsonencode({
-    tokens = []
-  })
+  value = "placeholder-replace-me"
   tags = merge(local.default_tags, { Backup = "true" })
 
   lifecycle {
