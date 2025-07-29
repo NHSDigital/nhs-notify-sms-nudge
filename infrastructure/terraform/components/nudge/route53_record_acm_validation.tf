537f1808f13c657af1015216423a85b556828901
Ian Hodges <ian.hodges1@nhs.net>
2025-07-29 13:34:35 +0100

    CCM-10152: add alias for cloudfront

--- infrastructure/terraform/components/nudge/route53_record_acm_validation.tf
+++ infrastructure/terraform/components/nudge/route53_record_acm_validation.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "acm_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options :
+    for dvo in aws_acm_certificate.aws_acm_certificate.static_assets_hosting.domain_validation_options :
     dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
--- infrastructure/terraform/components/nudge/route53_record_static_assets_hosting.tf
+++ infrastructure/terraform/components/nudge/route53_record_static_assets_hosting.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "static_assets_hosting" {
   zone_id = data.aws_route53_zone.main.zone_id
   type    = "CNAME"
   ttl     = 5
-  records = [aws_cloudfront_distribution.main.domain_name]
+  records = [aws_cloudfront_distribution.static_assets_hosting.domain_name]
 }
