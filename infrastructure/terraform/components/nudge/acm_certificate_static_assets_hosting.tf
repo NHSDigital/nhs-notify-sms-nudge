788873e94bd7c0e3d31ea23993809f2075c98b13
Ian Hodges <ian.hodges1@nhs.net>
2025-07-30 07:55:31 +0100

    CCM-10152: add alias for cloudfront

--- infrastructure/terraform/components/nudge/acm_certificate_static_assets_hosting.tf
+++ infrastructure/terraform/components/nudge/acm_certificate_static_assets_hosting.tf
@@ -11,5 +11,4 @@ resource "aws_acm_certificate" "static_assets_hosting" {
 resource "aws_acm_certificate_validation" "static_assets_hosting" {
   provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.static_assets_hosting.arn
-  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
