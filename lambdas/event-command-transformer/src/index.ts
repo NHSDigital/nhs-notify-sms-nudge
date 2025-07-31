2a0df2ca75b3884849e5dd349828fe810829b52c
Ian Hodges <ian.hodges1@nhs.net>
2025-07-31 13:38:27 +0100

    CCM-10152: fix dodgy merge

--- lambdas/event-command-transformer/src/index.ts
+++ lambdas/event-command-transformer/src/index.ts
@@ -1,33 +1,6 @@
-import type { SQSHandler } from 'aws-lambda';
-import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+// Lambda entrypoint
 
-const sqs = new SQSClient({});
+import { createContainer } from 'container/container';
+import { createHandler } from 'handler/sqs-handler';
 
-export const handler: SQSHandler = async (event) => {
-  const COMMANDS_QUEUE_URL = process.env.COMMANDS_QUEUE_URL!;
-  const EVENTS_DLQ_URL = process.env.EVENTS_DLQ_URL!;
-
-  for (const record of event.Records) {
-    try {
-      console.log('Received SQS message:', record.body);
-
-      // Forward message to the commands queue
-      await sqs.send(
-        new SendMessageCommand({
-          QueueUrl: COMMANDS_QUEUE_URL,
-          MessageBody: record.body,
-        }),
-      );
-    } catch (error) {
-      console.error('Error processing message, sending to DLQ:', error);
-
-      // Send the failed message to the events DLQ
-      await sqs.send(
-        new SendMessageCommand({
-          QueueUrl: EVENTS_DLQ_URL,
-          MessageBody: record.body,
-        }),
-      );
-    }
-  }
-};
+export const handler = createHandler(createContainer());
