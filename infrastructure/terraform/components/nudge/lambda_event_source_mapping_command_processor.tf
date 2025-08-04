0e2c7c097fdb537e9a597478080c0033284b9920
Ian Hodges <ian.hodges1@nhs.net>
2025-08-04 07:47:51 +0100

    CCM-10152: remove variable declared with its default value

--- infrastructure/terraform/components/nudge/lambda_event_source_mapping_command_processor.tf
+++ infrastructure/terraform/components/nudge/lambda_event_source_mapping_command_processor.tf
@@ -1,7 +1,6 @@
 resource "aws_lambda_event_source_mapping" "command_processor" {
   event_source_arn                   = module.sqs_command.sqs_queue_arn
   function_name                      = module.lambda_command_processor.function_name
-  enabled                            = true
   batch_size                         = var.queue_batch_size
   maximum_batching_window_in_seconds = var.queue_batch_window_seconds
   function_response_types = [
