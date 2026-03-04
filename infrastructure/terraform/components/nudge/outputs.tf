3a7435cbf61bcd646a8da4e216af610111cb37fe
aidenvaines-cgi <aiden.vaines@cgi.com>
2026-03-04 20:38:42 +0000

    CCM-14044 Unused output

--- infrastructure/terraform/components/nudge/outputs.tf
+++ infrastructure/terraform/components/nudge/outputs.tf
@@ -9,12 +9,3 @@ output "deployment" {
     component      = var.component
   }
 }
-
-# CloudWatch Anomaly Detection Alarm
-output "inbound_event_subscriber_anomaly_alarm" {
-  description = "Inbound event subscriber anomaly detection alarm details"
-  value = var.enable_event_anomaly_detection ? {
-    arn  = aws_cloudwatch_metric_alarm.inbound_event_subscriber_anomaly[0].arn
-    name = aws_cloudwatch_metric_alarm.inbound_event_subscriber_anomaly[0].alarm_name
-  } : null
-}
